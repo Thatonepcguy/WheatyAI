4bc4bf57a695eddd822e302012a28fefc11492e0
Wheat Studios <wileonards@gmail.com>
2026-07-05 19:52:40 -0500

    Keep only Modelfile

--- test_wheaty.py
+++
@@ -1,8 +0,0 @@
-print("Hello from Wheaty test script!")
-import torch
-print(f"Torch version: {torch.__version__}")
-import transformers
-print("Transformers imported!")
-from peft import LoraConfig
-print("Peft imported!")
-print("Wheaty is alive!")
